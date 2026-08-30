04466ecf1d58982e46656fbe56d1fd111390e6b7
404Vargas <abcstumijuanchi@gmail.com>
2026-08-30 16:34:58 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
+from app.core.database import Base, engine
+from app.requisitos.router import router as requisitos_router
 from fastapi import FastAPI
 
 app = FastAPI(
     title="DinamikUTB API",
     version="0.1.0"
 )
+
+Base.metadata.create_all(bind=engine)
+app.include_router(requisitos_router)
 
 @app.get("/")
 def root():

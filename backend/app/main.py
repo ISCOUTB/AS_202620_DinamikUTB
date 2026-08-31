f6ed7ca513ca5262d761fc28d0f538e8d0347ee6
404Vargas <abcstumijuanchi@gmail.com>
2026-08-30 20:17:35 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import Base, engine
 from app.requisitos.router import router as requisitos_router
 from fastapi import FastAPI
@@ -5,6 +6,13 @@ from fastapi import FastAPI
 app = FastAPI(
     title="DinamikUTB API",
     version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
